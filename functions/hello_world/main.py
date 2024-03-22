383129f8b08bfbbbb52dd849c881ef74f491b2bd
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-22 13:52:42 -0300

    first commit

--- functions/hello_world/main.py
+++ functions/hello_world/main.py
@@ -14,5 +14,5 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Hello World!"})
+        "body": json.dumps({"message": "Hello Capgemini!"})
     }
